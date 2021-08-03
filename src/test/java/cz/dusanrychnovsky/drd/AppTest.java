5a56ff989a2dc868ceff53a4a3a014f602b63e5c
Dusan Rychnovsky <dusan.rychnovsky@gmail.com>
2021-08-03 19:54:18 +0200

    Add JUnit.

---
+++ src/test/java/cz/dusanrychnovsky/drd/AppTest.java
@@ -0,0 +1,12 @@
+package cz.dusanrychnovsky.drd;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class AppTest {
+
+  @Test
+  public void test() {
+    assertEquals(true, true);
+  }
+}
